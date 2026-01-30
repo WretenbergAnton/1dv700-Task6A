6362d7ade085a82f1654d759132c802ba2c49e25
Anton Wretenberg <Anton.Wretenberg@outlook.com>
2026-01-30 12:40:17 +0100

    Add simple 8-bit hash function and random string generator

--- CollisionDemo.java
+++ CollisionDemo.java
@@ -1,6 +1,27 @@
+import java.util.Random;
+
 public class CollisionDemo {
+
+  public static int hash8(String line) {
+    int sum = 0;
+    for (char c : line.toCharArray()) {
+      sum += (int) c;
+    }
+    return sum % 256;
+  }
+
+  public static String randomString(Random rnd, int length) {
+    String chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < length; i++) {
+      sb.append(chars.charAt(rnd.nextInt(chars.length())));
+    }
+    return sb.toString();
+  }
   
   public static void main(String[] args) {
-      System.out.println("Collision demo starting");
+    Random rnd = new Random();
+    
+      System.out.println(randomString(rnd, 20));
   }
 }
